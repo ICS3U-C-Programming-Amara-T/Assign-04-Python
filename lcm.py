b4873356e88fb6714030b06808f70faf471fcc0a
Amara Tie <amara.tie@stu.ocsb.ca>
2025-05-09 05:30:19 +0000

    lcm.py

--- lcm.py
+++ lcm.py
@@ -6,4 +6,14 @@
 
 # This is a program calculates the LCM of 3 numbers!
 
+def lcm(number_1, number_2, number_3):
+    counter = 1
+    while True:
+        counter += 1
+        if (
+            counter % number_1 == 0
+            and counter % number_2 == 0
+            and counter % number_3 == 0
+        ):
+            return counter
 
